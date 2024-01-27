592957626d6783e7937951a2d560ff7a35df3a85
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-01-26 22:25:49 -0300

    add estrutura inicial projeto

---
+++ server/src/main/java/br/com/lcano/elodebito/ElodebitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lcano.elodebito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElodebitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElodebitoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/br/com/lcano/elodebito/ElodebitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lcano.elodebito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElodebitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
