87153b55e94fba3e719c56456db1121db364cb0b
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-01-28 02:11:24 -0300

    add endpoint para adicionar debito

--- server/src/main/java/br/com/lcano/elodebito/resource/DebitoResource.java
+++ server/src/main/java/br/com/lcano/elodebito/resource/DebitoResource.java
@@ -1,12 +1,11 @@
 package br.com.lcano.elodebito.resource;
 
 import br.com.lcano.elodebito.dto.DebitoDTO;
+import br.com.lcano.elodebito.dto.NovoDebitoDTO;
 import br.com.lcano.elodebito.service.DebitoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -23,5 +22,10 @@ public class DebitoResource {
     public ResponseEntity<List<DebitoDTO>> getAllDebitos() {
         List<DebitoDTO> debitoDTOS = debitoService.getAllDebitos();
         return ResponseEntity.ok(debitoDTOS);
+    }
+
+    @PostMapping("adicionar")
+    public ResponseEntity<String> adicionarDebito(@RequestBody NovoDebitoDTO data) {
+        return debitoService.adicionarDebito(data);
     }
 }
--- server/src/main/java/br/com/lcano/elodebito/service/DebitoService.java
+++ server/src/main/java/br/com/lcano/elodebito/service/DebitoService.java
@@ -1,23 +1,70 @@
 package br.com.lcano.elodebito.service;
 
 import br.com.lcano.elodebito.domain.Debito;
+import br.com.lcano.elodebito.domain.Pessoa;
 import br.com.lcano.elodebito.dto.DebitoDTO;
+import br.com.lcano.elodebito.dto.NovoDebitoDTO;
+import br.com.lcano.elodebito.dto.NovoDebitoParcelaDTO;
 import br.com.lcano.elodebito.repository.DebitoRepository;
+import br.com.lcano.elodebito.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DebitoService {
     @Autowired
     private final DebitoRepository debitoRepository;
 
-    public DebitoService(DebitoRepository debitoRepository) {
+    @Autowired
+    private  final DebitoParcelaService debitoParcelaService;
+
+    @Autowired
+    private final PessoaRepository pessoaRepository;
+
+    public DebitoService(DebitoRepository debitoRepository,
+                         DebitoParcelaService debitoParcelaService,
+                         PessoaRepository pessoaRepository) {
         this.debitoRepository = debitoRepository;
+        this.debitoParcelaService = debitoParcelaService;
+        this.pessoaRepository = pessoaRepository;
     }
 
     public List<DebitoDTO> getAllDebitos() {
         List<Debito> debitos = debitoRepository.findAll();
         return debitos.stream().map(DebitoDTO::converterParaDTO).toList();
+    }
+
+    public ResponseEntity<String> adicionarDebito(NovoDebitoDTO data) {
+        try {
+            Optional<Pessoa> optionalPessoa = getPessoaById(data.getIdPessoa());
+
+            if (optionalPessoa.isPresent()) {
+                Pessoa pessoa = optionalPessoa.get();
+
+                Debito novoDebito = new Debito();
+                novoDebito.setPessoa(pessoa);
+                novoDebito.setDataLancamento(data.getDataLancamento());
+                debitoRepository.save(novoDebito);
+
+                List<NovoDebitoParcelaDTO> parcelaDTOS = data.getParcelasDTO();
+                for (NovoDebitoParcelaDTO parcelaDTO : parcelaDTOS) {
+                    debitoParcelaService.adicionarParcela(parcelaDTO, novoDebito);
+                }
+            } else {
+                throw new RuntimeException("Pessoa nao encontrada");
+            }
+
+            return ResponseEntity.ok("Novo débito adicionado com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao adicionar o débito: " + e.getMessage());
+        }
+    }
+
+    private Optional<Pessoa> getPessoaById(Long id) {
+        return pessoaRepository.findById(id);
     }
 }
