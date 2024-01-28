0c35b3bd361ff039f70c7a8ea9516a486fc69303
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-01-28 14:16:56 -0300

    add MensagemUtils

---
+++ server/src/main/java/br/com/lcano/elodebito/util/MensagemUtils.java
@@ -0,0 +1,7 @@
+package br.com.lcano.elodebito.util;
+
+public class MensagemUtils {
+    public static final String ERRO_GENERICO = "Ocorreu um erro interno no servidor.";
+
+    public static final String PESSOA_NAO_ENCONTRADA_COM_ID = "Pessoa não encontrada com o id: %d.";
+}
