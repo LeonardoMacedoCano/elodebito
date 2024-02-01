59cd808921fef7768bf2f6b1662eab708e7c9339
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-02-01 01:13:24 -0300

    atualizar debito service e add teste

--- server/src/main/java/br/com/lcano/elodebito/service/DebitoService.java
+++ server/src/main/java/br/com/lcano/elodebito/service/DebitoService.java
@@ -1,13 +1,10 @@
 package br.com.lcano.elodebito.service;
 
 import br.com.lcano.elodebito.domain.Debito;
-import br.com.lcano.elodebito.domain.Pessoa;
 import br.com.lcano.elodebito.dto.DebitoDTO;
 import br.com.lcano.elodebito.dto.NovoDebitoDTO;
 import br.com.lcano.elodebito.dto.NovoDebitoParcelaDTO;
 import br.com.lcano.elodebito.repository.DebitoRepository;
-import br.com.lcano.elodebito.repository.PessoaRepository;
-import br.com.lcano.elodebito.util.CustomException;
 import br.com.lcano.elodebito.util.CustomSuccess;
 import br.com.lcano.elodebito.util.MensagemUtils;
 import jakarta.transaction.Transactional;
@@ -15,25 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DebitoService {
-    @Autowired
     private final DebitoRepository debitoRepository;
-
-    @Autowired
     private  final DebitoParcelaService debitoParcelaService;
+    private final PessoaService pessoaService;
 
     @Autowired
-    private final PessoaRepository pessoaRepository;
-
     public DebitoService(DebitoRepository debitoRepository,
                          DebitoParcelaService debitoParcelaService,
-                         PessoaRepository pessoaRepository) {
+                         PessoaService pessoaService) {
         this.debitoRepository = debitoRepository;
         this.debitoParcelaService = debitoParcelaService;
-        this.pessoaRepository = pessoaRepository;
+        this.pessoaService = pessoaService;
     }
 
     public List<DebitoDTO> getAllDebitos() {
@@ -42,26 +34,25 @@ public class DebitoService {
     }
 
     @Transactional
-    public ResponseEntity<Object> adicionarDebito(NovoDebitoDTO data) {
-        Optional<Pessoa> optionalPessoa = getPessoaById(data.getIdPessoa());
-
-        if (optionalPessoa.isPresent()) {
-            Debito novoDebito = new Debito();
-            novoDebito.setPessoa(optionalPessoa.get());
-            novoDebito.setDataLancamento(data.getDataLancamento());
-            debitoRepository.save(novoDebito);
-
-            List<NovoDebitoParcelaDTO> parcelaDTOS = data.getParcelasDTO();
-            for (NovoDebitoParcelaDTO parcelaDTO : parcelaDTOS) {
-                debitoParcelaService.adicionarParcela(parcelaDTO, novoDebito);
-            }
-            return CustomSuccess.buildResponseEntity(MensagemUtils.DEBITO_ADICIONADO_COM_SUCESSO);
-        } else {
-            throw new CustomException.PessoaNaoEncontradaComIdException(data.getIdPessoa());
-        }
+    public ResponseEntity<Object> adicionarNovoDebito(NovoDebitoDTO data) {
+        Debito novoDebito = criarDebito(data);
+        salvarDebito(novoDebito);
+        adicionarNovasParcelas(novoDebito, data.getParcelasDTO());
+        return CustomSuccess.buildResponseEntity(MensagemUtils.DEBITO_ADICIONADO_COM_SUCESSO);
     }
 
-    private Optional<Pessoa> getPessoaById(Long id) {
-        return pessoaRepository.findById(id);
+    private Debito criarDebito(NovoDebitoDTO data) {
+        Debito novoDebito = new Debito();
+        novoDebito.setPessoa(pessoaService.getPessoaById(data.getIdPessoa()));
+        novoDebito.setDataLancamento(data.getDataLancamento());
+        return novoDebito;
+    }
+
+    private void adicionarNovasParcelas(Debito debito, List<NovoDebitoParcelaDTO> parcelasDTO) {
+        parcelasDTO.forEach(parcelaDTO -> debitoParcelaService.adicionarNovaParcela(parcelaDTO, debito));
+    }
+
+    public void salvarDebito(Debito debito) {
+        debitoRepository.save(debito);
     }
 }
---
+++ server/src/test/java/br/com/lcano/elodebito/service/DebitoServiceTest.java
@@ -0,0 +1,92 @@
+package br.com.lcano.elodebito.service;
+
+import br.com.lcano.elodebito.domain.Debito;
+import br.com.lcano.elodebito.domain.Pessoa;
+import br.com.lcano.elodebito.dto.DebitoDTO;
+import br.com.lcano.elodebito.dto.NovoDebitoDTO;
+import br.com.lcano.elodebito.dto.NovoDebitoParcelaDTO;
+import br.com.lcano.elodebito.repository.DebitoRepository;
+import br.com.lcano.elodebito.util.MensagemUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class DebitoServiceTest {
+    @Mock
+    private DebitoRepository debitoRepository;
+
+    @Mock
+    private DebitoParcelaService debitoParcelaService;
+
+    @Mock
+    private PessoaService pessoaService;
+
+    @InjectMocks
+    private DebitoService debitoService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        debitoService = new DebitoService(debitoRepository, debitoParcelaService, pessoaService);
+    }
+
+    @Test
+    public void testGetAllDebitos() {
+        Pessoa pessoa = new Pessoa();
+        List<Debito> debitos = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            Debito debito = new Debito();
+            debito.setId((long) i);
+            debito.setPessoa(pessoa);
+            debito.setDataLancamento(new Date());
+            debitos.add(debito);
+        }
+
+        when(debitoRepository.findAll()).thenReturn(debitos);
+        List<DebitoDTO> debitoDTOs = debitoService.getAllDebitos();
+        assertEquals(debitos.size(), debitoDTOs.size());
+    }
+
+
+    @Test
+    public void testAdicionarNovoDebito() {
+        List<NovoDebitoParcelaDTO> parcelasDTO = new ArrayList<>();
+        for (int i = 1; i <= 12; i++) {
+            NovoDebitoParcelaDTO novaParcelaDTO = new NovoDebitoParcelaDTO();
+            novaParcelaDTO.setNumero(i);
+            novaParcelaDTO.setDataVencimento(new Date());
+            novaParcelaDTO.setSituacao('A');
+            novaParcelaDTO.setValor(10.0);
+            parcelasDTO.add(novaParcelaDTO);
+        }
+
+        NovoDebitoDTO novoDebitoDTO = new NovoDebitoDTO();
+        novoDebitoDTO.setIdPessoa(1L);
+        novoDebitoDTO.setParcelasDTO(parcelasDTO);
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setId(1L);
+        Debito debito = new Debito();
+
+        when(pessoaService.getPessoaById(pessoa.getId())).thenReturn(pessoa);
+        when(debitoRepository.save(debito)).thenReturn(debito);
+
+        ResponseEntity<Object> response = debitoService.adicionarNovoDebito(novoDebitoDTO);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(Map.of("success", MensagemUtils.DEBITO_ADICIONADO_COM_SUCESSO), response.getBody());
+        verify(debitoParcelaService, times(12)).adicionarNovaParcela(any(), any());
+    }
+}
