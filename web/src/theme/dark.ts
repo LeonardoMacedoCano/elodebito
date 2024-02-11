6f0ef87a9217c8a0d27c3ec9fc160d614531a7e9
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-02-10 23:00:28 -0300

    add tema dark

---
+++ web/src/theme/dark.ts
@@ -0,0 +1,17 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#131629',
+        secondary: '#1f233d',
+        tertiary: '#2f3557',
+
+        white: '#FFF',
+        black: '#000',
+        gray: '#888',
+
+        success: '#4E41F0',
+        info: '#F7931B',
+        warning: '#E44C4E',
+    },
+};
