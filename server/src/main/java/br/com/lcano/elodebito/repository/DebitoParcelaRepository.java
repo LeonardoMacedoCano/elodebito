fb1b0a1671bd28e50fe8afd9b8feb3c8bced6bea
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-02-13 15:56:18 -0300

    retirar metodo nao usado

--- server/src/main/java/br/com/lcano/elodebito/repository/DebitoParcelaRepository.java
+++ server/src/main/java/br/com/lcano/elodebito/repository/DebitoParcelaRepository.java
@@ -1,16 +1,12 @@
 package br.com.lcano.elodebito.repository;
 
-import br.com.lcano.elodebito.domain.Debito;
 import br.com.lcano.elodebito.domain.DebitoParcela;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface DebitoParcelaRepository extends JpaRepository<DebitoParcela, Long> {
-    List<DebitoParcela> findByDebito(Debito debito);
-
     DebitoParcela findByDebitoIdAndNumero(Long id, int numero);
 
     @Query("SELECT SUM(dp.valor) FROM DebitoParcela dp")
